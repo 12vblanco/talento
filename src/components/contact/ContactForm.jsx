bc2d7031ece88e7e2d14a4b36a16b4dcae13ec01
Victor Blanco <12vblanco@gmail.com>
2023-05-30 21:30:42 +0800

    burger

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import CTAButton from "../misc/CTABUtton";
+import CTAButton from "../misc/CTAButton";
 
 const ContactForm = () => {
   return (
